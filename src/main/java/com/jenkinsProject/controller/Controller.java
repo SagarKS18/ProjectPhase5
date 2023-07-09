4950bb0e3a6e88751e301e141f0d68b687855b33
Sagar <mail.sagarsolapur@gmail.com>
2023-07-09 15:36:47 +0530

    first commit

---
+++ src/main/java/com/jenkinsProject/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.jenkinsProject.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
